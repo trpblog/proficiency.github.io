03c0f48f23f788065a5dcb7f138bbbcbbd62a5d7
Trishita Roy <trishita.iips@gmail.com>
2020-05-02 20:40:40 +0530

    My TRP Website

---
+++ aboutus.php
@@ -0,0 +1,119 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navbar.php"); ?>
+
+
+<section class="section bg-WhatisOurProcedure">
+    <div class="container">
+      <div class="row">
+      <div class="col-md-12 text-center">
+             <h2 class="section-title">
+                 <span class="text-red">What is</span>
+                        Our Procedure
+             </h2>
+             <div class="underline mr-auto ml-auto mb-6"></div>
+                <p class="section-subtitle text-center text-black">
+                Young and daring, enthusiast, with high energy level and stamina, and high caliber to walk on the hard path of training can become a successful aviator. 
+                </p>
+             
+        
+      </div>
+    </div>
+      </div>
+      <div class="row">
+        
+      
+      <div class="col-md-4 mt-3">
+          <div class="card">
+              <div class="card-body">
+                 <h5 class="card-title">RESUME PREPARATION</h5>
+                 <div class="underline mb-3"></div>
+                 <p class="card-text">We strongly believe that Resume should speak first. 
+                    For all our students, with the support of our real-time trainers,
+                    every resume is written to get shortlisted in employer companies. 
+                    We spend considerable amount of time 
+                    and effort to draft our students resumes in a
+                     better to make it more presentable manner..</p>
+                 <a href="contactus.php" class="btn btn-primary">Contact with us</a>
+       </div>
+        </div>
+        </div>
+
+        <div class="col-md-4 mt-3">
+          <div class="card">
+              <div class="card-body">
+                 <h5 class="card-title">MOCK INTERVIEWS</h5>
+                 <div class="underline mb-3"></div>
+                 <p class="card-text">With the Support of Our
+                  industry experienced trainers and 
+                   the industry professional network,
+                   we organize 3-5 mock interviews for every 
+                   student to aware their strengths and weakness. 
+                   The mock interviews include HR round,
+                   Technical round, Management Round with complete evaluation technical, 
+                   communication, behavioral skills.
+                  </p>       
+                  <a href="contactus.php" class="btn btn-primary">Contact with us</a>
+       </div>
+        </div>
+        </div>
+
+        <div class="col-md-4 mt-3">
+          <div class="card">
+              <div class="card-body">
+                 <h5 class="card-title">PLACEMENT TRAINING IN INDIA - 100% PLACEMENT ASSISTANCE</h5>
+                 <div class="underline mb-3"></div>
+                 <p class="card-text">TRP Proficiency and Skills offers 
+                 more than 10 training courses with 100% placement support in pan India.
+                    We focused on Job oriented training programs which helps our 
+                    students to get a job as soon as they complete 
+                    our placement trainings in kolkata.Through our associated
+                     Network and support from InterviewDesk,
+                     Our training programs and course contents are designed such
+                      a way to achieve every student's career goal. 
+                     
+              </p>
+                 <a href="contactus.php" class="btn btn-primary">Contact with us</a>
+       </div>
+        </div>
+        </div>
+
+        
+      </div>
+
+       
+</section>
+
+
+
+
+
+
+
+<section class="section">
+    <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+                <h2 class="section-title text-center">
+                  <span class="text-red"> Modern Way to make your career </span>
+                    by TRP Proficiency and Skills
+                </h2>
+                <div class="underline mr-auto ml-auto mb-2"></div>
+                <p class="section-subtitle text-center">
+                Our capabilities are completely scalable and our processes provide industry-leading results, we conduct a full search for the best candidates for your open position and then interview & pre-qualify them. This round is taken by industry specialists as well as HR consultants to evaluate the candidate on dual criteria of professional knowledge (like professional expertise and employment goals) and individual fit in the prospective organization.
+                </p>
+            </div>
+            <div class="col-md-12">
+            <p class="text-center">
+              We provide you with nonpareil solutions to expand your reach to every corner you desire.
+              We combine our deep understanding of the Students internal & external requirement with our strategic approach,
+              providing a one-stop solution to get your brand on the go.
+              Our approach focuses on how we deliver operational excellence to provide viable, 
+              cost-effective technology-enabled solutions to the most challenging Management questions. 
+              We reciprocate through our work entirely with a progressive mindset. “YOU TRUST US, WE CARE”.
+              We cater to every technological need for our students with a modern touch, keeping the world in motion.
+            </p>
+         </div>
+    </div>
+      </div>
+</section>
+<?php include("includes/footer.php") ?>
